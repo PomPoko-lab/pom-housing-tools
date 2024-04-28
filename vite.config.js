aaf06f5aa0c93ce7b0d05ff7de9a89f17c757d35
Kenny Cao <ckennx@gmail.com>
2024-04-27 23:00:38 -0500

    add gh pages deployments and adjusted title

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+    base: '/pom-housing-tools/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
